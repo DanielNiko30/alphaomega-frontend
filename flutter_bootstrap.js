72bc37e34c87a700c7558c2afb0d35931f77a819
DanielNiko30 <danielniko300703@gmail.com>
2025-12-09 22:03:21 +0700

    build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2921330174"
+    serviceWorkerVersion: "2489323073"
   }
 });
